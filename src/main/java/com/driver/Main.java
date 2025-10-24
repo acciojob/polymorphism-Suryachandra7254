b1f8f8043c9ce77bedf27493c8c1482007ddfc7c
NAGA SURYACHANDRA RAO YELUGULA <yelugulasuryachandra@gmail.com>
2025-10-24 10:55:24 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+   public  static class product{
+     
+      public int product(int x, int y) {
+            return x * y;
+        }
+     public int product(int x, int y, int z){
+             return x * y * z;
+     }
+     public double product(double x, double y){
+             return x * y;
+     }
+   }
+
+  public static void main(String [] args){
+
+    product p = new product();
+
+    System.out.println("Product of 2 integers: " + p.product(4, 5));         
+    System.out.println("Product of 3 integers: " + p.product(2, 3, 4));    
+    System.out.println("Product of 2 doubles: " + p.product(3.5, 2.5));
+  }
+     
+
 }
